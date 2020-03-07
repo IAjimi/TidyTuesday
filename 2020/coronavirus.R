1b61db1f81a8fdbb34db4ee9d4ba66f407434a21
IAjimi <ia767@nyu.edu>
2020-03-07 12:03:53 -0500

    initial commit

---
+++ 2020/coronavirus.R
@@ -0,0 +1,61 @@
+confirmed_cases <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+
+confirmed_cases <- confirmed_cases %>% 
+  gather(date_key, date_val, -Lat, -Long, - `Country/Region`, -`Province/State`) %>%
+  mutate(date_key = as.Date(date_key, "%m/%d/%y"))
+
+
+### World Map ####
+#partly from https://stackoverflow.com/questions/30706124/plotting-the-world-map-in-r
+WorldData <- map_data('world') %>% filter(region != "Antarctica") %>% fortify
+
+ggplot() +
+  geom_map(data = WorldData, map = WorldData,
+           aes(x = long, y = lat, group = group, map_id=region),
+           fill = "white", colour = "#7f7f7f", size=0.5) + 
+  coord_map("rectangular", lat0=0, xlim=c(-180,180), ylim=c(-60, 90)) +
+  geom_point(data = filter(confirmed_cases, date_key == "2020-03-04"), 
+             aes(Long, Lat, size = date_val, color = "#db4551"), alpha = 0.6) +
+  #scale_color_manual(values = c("#db4551", '#455edb')) +
+  guides(size = FALSE, color = FALSE) +
+  scale_y_continuous(breaks=c()) +
+  scale_x_continuous(breaks=c()) +
+  labs(title="COVID-19", subtitle = "Confirmed Cases, 2020-03-04",
+       x="", y="", color = "", caption = "Source: John Hopkins University") +
+  theme_bw()
+
+### DISEASE SPREAD SINCE FIRST CONFIRMED CASE
+confirmed_cases_by_country <- confirmed_cases %>% 
+  group_by(`Country/Region`, date_key) %>% 
+  summarise(date_val = sum(date_val)) %>%
+  ungroup()
+
+most_cases_country <- confirmed_cases_by_country %>% 
+  filter(date_key == max(.$date_key) & `Country/Region` != "Others") %>% 
+  arrange(desc(date_val)) %>% 
+  top_n(5, date_val)
+
+confirmed_cases_spread <- confirmed_cases_by_country %>% 
+  split(.$`Country/Region`) %>%
+  map(filter, date_val > 0) %>%
+  map(function(x) mutate(x, x_axis = c(1:nrow(x))))
+
+confirmed_cases_spread <- do.call("rbind.data.frame", confirmed_cases_spread)
+
+confirmed_cases_spread %>%
+  filter(`Country/Region` %in% most_cases_country$`Country/Region`) %>%
+  mutate(
+    colourin = case_when(
+      `Country/Region` == "Mainland China" ~ "#003399",
+      `Country/Region` == "Iran" ~ "#FF2B4F",
+      `Country/Region` == "Italy" ~ "#fcab27",
+      `Country/Region` == "South Korea" ~ "#3686d3",
+      T ~ "darkgray")
+  ) %>% 
+  ggplot(aes(x_axis, date_val, color = colourin)) +
+  geom_line(size = 1) +
+  scale_color_identity() +
+  #theme_economist() +
+  theme_bw() +
+  labs(x = "Days since 1st Recorded Case", y = "# Confirmed Cases", color = "Country",
+       title = "Covid-19", subtitle = "Spread Since 1st Confirmed Case")
