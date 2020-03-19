808325c9366d44c29d71c18de6b6fc35f818eca9
IAjimi <ia767@nyu.edu>
2020-03-19 13:33:46 -0400

    week 6 update

--- 2020/week_6/week_6.R
+++ 2020/week_6/week_6.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 attendance <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/attendance.csv')
 standings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/standings.csv')
 games <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/games.csv')
@@ -37,14 +39,22 @@ path_to_victory <- games %>%
     week == "SuperBowl" ~ 21,
     T ~ as.numeric(week)
   )) %>%
-  mutate(pts_home_team = if_else(home_team == winner, pts_win - pts_loss, pts_loss - pts_win))
+  mutate(pts_differential = if_else(home_team == winner, pts_win - pts_loss, pts_loss - pts_win))
+
+superbowl_teams <- path_to_victory %>%
+  filter(week == "SuperBowl") %>%
+  mutate(superbowl_bool = T) %>%
+  select(home_team, away_team, year, superbowl_bool) %>%
+  gather(team_key, team_value, c(home_team, away_team))
 
 path_to_victory %>%
+  left_join(superbowl_teams, by = c("home_team" = "team_value", "year")) %>%
   filter(year >= 2015) %>%
-  ggplot(aes(game_week, pts_home_team, group = team)) +
+  ggplot(aes(game_week, pts_differential , group = team)) +
   geom_path(data = . %>% filter(sb_winner != "Won Superbowl"), alpha = 0.5, color = "darkgrey") +
-  geom_path(data = . %>% filter(sb_winner == "Won Superbowl"), size = 1, alpha = 0.8, color = "red") +
-  geom_hline(yintercept = 0, alpha = 0.5) +
+  geom_path(data = . %>% filter(superbowl_bool == T), size = 1, alpha = 0.8, color = "#FF2B4F") +
+  geom_path(data = . %>% filter(sb_winner == "Won Superbowl"), size = 1, alpha = 0.8, color = "#3686d3") +
+  geom_hline(yintercept = 0, alpha = 0.5) + 
   guides(color = FALSE) +
   labs(x = "Week", y = "Points Differential",
        title = "Path to Victory", subtitle = "Point Differential for Both Superbowl Teams By Season") +
