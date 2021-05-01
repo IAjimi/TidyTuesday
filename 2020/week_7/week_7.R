6a03cf092a285cb07b46c922f4715b87c9cf1e70
IAjimi <ia767@nyu.edu>
2021-05-01 08:31:23 -0400

    adding quick models for week 12 2021 and week 7 2020

---
+++ 2020/week_7/week_7.R
@@ -0,0 +1,134 @@
+# TIDYTUESDAY 2020 WEEK 7
+# Exploring tidymodels with https://www.tidymodels.org/start/case-study/
+# Predicting hotel cancellations & no-shows
+
+#### SETUP ####
+library(tidyverse)
+library(tidymodels)
+
+hotels <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-11/hotels.csv')
+hotels <- hotels %>% 
+  select(-is_canceled, - reservation_status_date) %>%
+  mutate(reservation_status = if_else(reservation_status %in% c('No-Show', 'Canceled'), '1', '0'),
+         reservation_status = as.factor(reservation_status),
+         children = replace_na(children, 0)
+         )
+
+
+#### EDA ####
+hotels %>% summary()
+
+hotels %>% 
+  count(hotel) %>% 
+  mutate(prop = n/sum(n))
+
+hotels %>% 
+  count(meal) %>% 
+  mutate(prop = n/sum(n))
+
+hotels %>% # maybe regroup to continent?
+  count(country) %>% 
+  mutate(prop = n/sum(n)) %>%
+  arrange(desc(prop))
+
+hotels %>% 
+  count(market_segment) %>% 
+  mutate(prop = n/sum(n)) %>%
+  arrange(desc(prop))
+
+hotels %>% 
+  count(company) %>% # mostly NULL
+  mutate(prop = n/sum(n)) %>%
+  arrange(desc(prop))
+
+#### MODELS ####
+set.seed(15)
+splits <- initial_split(hotels, strata = reservation_status)
+other <- training(splits)
+validation_set <- validation_split(
+                            other, 
+                            strata = reservation_status, 
+                            prop = 0.8)
+test  <- testing(splits)
+
+
+### Log Reg w/ penalization
+lr_mod <- 
+  logistic_reg(penalty = tune(), mixture = 1) %>% 
+  set_engine("glmnet")
+
+holidays <- c("AllSouls", "AshWednesday", "ChristmasEve", "Easter", 
+              "ChristmasDay", "GoodFriday", "NewYearsDay", "PalmSunday")
+
+lr_recipe <- 
+  recipe(reservation_status ~ ., data = other) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>% # converts char. & factors to dummies
+  step_zv(all_predictors()) %>% # removes cols w/ unique value
+  step_normalize(all_predictors()) # normalizes numeric predictors
+
+lr_workflow <- 
+  workflow() %>% 
+  add_model(lr_mod) %>% 
+  add_recipe(lr_recipe)
+
+lr_reg_grid <- tibble(penalty = 10^seq(-4, -1, length.out = 30)) # only 1 tuning param for this
+
+lr_res <- 
+  lr_workflow %>% 
+  tune_grid(validation_set,
+            grid = lr_reg_grid,
+            control = control_grid(save_pred = TRUE),
+            metrics = metric_set(roc_auc))
+
+lr_res$.notes[[1]][[1]]
+
+lr_plot <- 
+  lr_res %>% 
+  collect_metrics() %>% 
+  ggplot(aes(x = penalty, y = mean)) + 
+  geom_point() + 
+  geom_line() + 
+  ylab("Area under the ROC Curve") +
+  scale_x_log10(labels = scales::label_number())
+
+lr_plot 
+# better model performance at smaller penalty values
+#  majority of the predictors are important to the model
+# will pick model w best perf and highest penalty
+
+lr_best <- lr_res %>% 
+  collect_metrics() %>% 
+  filter(round(mean, 3) == round(max(mean), 3)) %>% 
+  arrange(penalty) %>% 
+  tail(1)
+
+lr_best # 0.913 roc_auc, 0.000530 penalty
+
+lr_auc <- 
+  lr_res %>% 
+  collect_predictions(parameters = lr_best) %>% 
+  roc_curve(reservation_status, .pred_0) %>% # likelihood of not cancelled
+  mutate(model = "Logistic Regression")
+
+autoplot(lr_auc)
+
+### On test
+glmn_fit <- 
+  logistic_reg(penalty = 0.000530, mixture = 1) %>% 
+  set_engine("glmnet")
+
+tuned_glmn_workflow <- 
+  lr_workflow %>% 
+  update_model(glmn_fit)
+
+last_fit <- 
+  tuned_glmn_workflow %>% 
+  last_fit(splits)
+
+last_fit %>% 
+  collect_metrics() # 83% accuracy, 0.914 ROC AUC on test
+
+last_fit %>% 
+  collect_predictions() %>% 
+  roc_curve(reservation_status, .pred_0) %>% 
+  autoplot()
--- 2021/week_12/week_12.R
+++ 2021/week_12/week_12.R
@@ -1,4 +1,6 @@
+# TIDYTUESDAY 2021 WEEK 12
 # Exploring tidymodels w/ https://www.tidymodels.org/learn/models/parsnip-ranger-glmnet/
+# Predicting metascore
 
 #### SETUP ####
 library(tidyverse)
@@ -8,6 +10,7 @@ video_games <- readr::read_csv("https://raw.githubusercontent.com/rfordatascienc
 games <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-16/games.csv')
 
 #### PRE PROCESSING ####
+# Simplify games df
 games <- games %>%
   group_by(gamename) %>%
   summarise(
@@ -15,6 +18,7 @@ games <- games %>%
     peak = max(peak) # peak number of players
     )
 
+# Add to main df
 combined_df <- games %>% 
   rename(game = gamename) %>%
   left_join(video_games, by = c("game")) %>%
@@ -23,7 +27,7 @@ combined_df <- games %>%
     ) %>% 
   filter(!is.na(metascore))
 
-# Add dev size, dev avg score
+# Add developper metrics: size, avg score
 dev_score <- combined_df %>%
   group_by(developer) %>%
   summarise(dev_size = n(),
@@ -83,6 +87,7 @@ combined_df <- combined_df %>%
 df_split <- initial_split(combined_df)
 train <- training(df_split)
 test  <- testing(df_split)
+folds <- vfold_cv(train, v = 10)
 
 preds <- c('avg', 'release_date', 'dev_avg', 'average_playtime')
 
@@ -105,7 +110,7 @@ test_pred_df <- test %>%
   )
 
 test_pred_df %>% slice(1:5)
-test_pred_df %>% metrics(truth = metascore, estimate = .pred) # 4.25
+test_pred_df %>% metrics(truth = metascore, estimate = .pred)
 
 ### LM
 # pre-processing -- only makes a tiny difference (rmse down to 3.35 from 3.37)
@@ -128,19 +133,70 @@ glmn_fit <-
 # process test
 test_normalized <- bake(norm_recipe, new_data = test, all_predictors())
 
-# join both predictions
+### TUNED TREE
+tune_spec <- decision_tree(
+  cost_complexity = tune(),
+  tree_depth = tune(),
+  min_n = tune()
+) %>%
+  set_engine("rpart") %>%
+  set_mode("regression")
+
+tree_grid <- grid_regular(cost_complexity(),
+                          tree_depth(),
+                          min_n(),
+                          levels = 5) # will try 5 values for each
+
+tree_wf <- workflow() %>%
+  add_model(tune_spec) %>%
+  add_formula(metascore ~ avg + peak + price + average_playtime + dev_avg)
+
+tree_res <- 
+  tree_wf %>% 
+  tune_grid(
+    resamples = folds,
+    grid = tree_grid
+  )
+
+tree_res # list of results by split
+
+tree_res %>% 
+  collect_metrics() # aggregating results by .config over all splits 
+
+tree_res %>%
+  show_best("rmse") # shows top 5 by metric by default
+
+best_tree <- tree_res %>%
+  select_best("rmse") # selects top by rmse
+
+final_wf <- 
+  tree_wf %>% 
+  finalize_workflow(best_tree) # final workflow
+
+final_tree <- 
+  final_wf %>%
+  fit(data = train) # final tree, ends up with mostly dev_avg, sometimes peak & average_playtime
+
+#### EVALUATE MODELS ####
+# join predictions
 test_results <- 
   test_pred_df %>%
   rename(`random forest` = .pred) %>%
   bind_cols(
     predict(glmn_fit, new_data = test_normalized) %>%
       rename(glmnet = .pred)
+  ) %>%
+  bind_cols(
+    predict(final_tree, new_data = test) %>%
+      rename(tuned_tree = .pred)
   )
 
-# compare both 
+# compare -- glmnet quickest & most accurate
 test_results %>% metrics(truth = metascore, estimate = glmnet) 
+test_results %>% metrics(truth = metascore, estimate = tuned_tree) 
 test_results %>% metrics(truth = metascore, estimate = `random forest`) 
 
+# plot comparison
 test_results %>% 
   ungroup() %>%
   select(-game) %>% 
@@ -155,7 +211,8 @@ test_results %>%
 test_errors <- test_results %>%
   mutate(
     rf_error = metascore - `random forest`,
-    glmnet_error = metascore - glmnet
+    glmnet_error = metascore - glmnet,
+    tuned_error = metascore - tuned_tree
     )
 
 test_errors %>%
